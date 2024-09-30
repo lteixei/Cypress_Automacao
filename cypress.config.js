a826588ef4a2ea31f011f68e6444c5a3810e7cde
Leonardo <lteixei@hotmail.com>
2024-09-30 16:31:29 -0300

    cypres config js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     // Suas configurações de teste
-  },
   reporter: 'cypress-junit',
   reporterOptions: {
     mochaFile: 'results/junit/results-[hash].xml', // Caminho para os resultados dos testes
