57d82aa9850d4ae5aab0efbb19dade5ff7ef7c77
LEONARDO TEIXEIRA <LTEIXEI@HOTMAIL.COM>
2025-07-08 14:08:12 -0300

    atu

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  e2e: {
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    supportFile: false
+  },
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/reports/mochawesome',
     overwrite: false,
     html: false,
     json: true
-  },
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  }
 });
