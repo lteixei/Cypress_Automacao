c2da14743be486e83cdf842e65e48137c4a51c18
LEONARDO TEIXEIRA <LTEIXEI@HOTMAIL.COM>
2025-07-09 14:53:26 -0300

    atu

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
 
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.{js,ts}",
-    supportFile: false
+    supportFile: false,
+    setupNodeEvents(on, config) {
+    allureWriter(on, config);
+    },
   },
   reporter: 'mochawesome',
   reporterOptions: {
