18098b7ba47a2896aa0758a30092c942083fc4e0
LEONARDO TEIXEIRA <LTEIXEI@HOTMAIL.COM>
2025-08-22 13:39:22 -0300

    atu

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,10 @@
+// Importando o defineConfig do Cypress
 const { defineConfig } = require('cypress');
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  videoCompression: true,
   e2e: {
-    setupNodeEvents(on, config) {
-      allureWriter(on, config); // só o writer é necessário
-      return config;
-    },
-    baseUrl: 'http://localhost:8081', // ou sua base real
+    // Configuração dos arquivos de teste
+    specPattern: 'cypress/integration/**/*.spec.js',  // Caminho correto para os testes
+    baseUrl: 'http://localhost:8081', // Base URL, altere conforme necessário
   },
-  env: {
-    allure: true,
-},
 });
---
+++ cypress/integration/ambientes.spec.js
@@ -0,0 +1,13 @@
+// <reference types="cypress" />
+describe('Livraria da Travessa', () => {
+    before(() => {
+      cy.visit('https://demowebshop.tricentis.com/')
+      cy.screenshot()
+    })
+    it('Novo Usuário', () =>{
+      //
+  //LOGANDO NO SITE  
+     
+      
+  })
+})
